1de6a1fa2756cd5d8c7baf746fcd10652f453704
sreeshma33 <sreeshmaunnikrishnan33@gmail.com>
2025-10-22 09:55:09 +0530

    [FIX]

--- cash_denomination/__manifest__.py
+++ cash_denomination/__manifest__.py
@@ -1,24 +1,13 @@
 # -*- coding: utf-8 -*-
 {
     'name': "cash_denomination",
-    'version': "1.5",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",#????  please change
-
-    'description': """
-      Cash Denomination
-    """,
-
-    'author': "iCodeBees",
-    'website': "https://www.yourcompany.com",
-    # change website and category
-    'category': 'Uncategorized',
-    'version': '0.1',
-
+    'summary': "Cash Denomination",
+    'description': "Cash Denomination",
+    'category': 'Website',
+    'version': '0.2',
     'depends': ['base','account','website'],
-
     'data': [
-        'security/ir.model.access.csv', 
+        'security/ir.model.access.csv',
         'views/cash_denomination_views.xml',
         'views/cash_counter_views.xml',
         'views/cash_transfer_views.xml',
@@ -29,7 +18,4 @@
                 'cash_denomination/static/src/js/cash_denomination.js',
             ],
         },
-
-
 }
-
